fe73b98bbb92a15296436e1a146637575ea6d421
kiryl_p <kiryl.pashkouski@gmail.com>
2019-12-29 18:27:01 -0500

    no message

---
+++ basics.py
@@ -0,0 +1,19 @@
+# a= 6
+# b= 2
+
+# print (a/b)
+
+cities = ['Tokyo','Los Angeles','New York','San Francisco']
+
+print(cities)
+
+print(cities[1])
+
+city_population = {
+    'Tokyo': 13350000, 
+    'Los Angeles': 18550000,
+    'New York City': 8400000,
+    'San Francisco': 1837442,
+}
+
+print(city_population)
